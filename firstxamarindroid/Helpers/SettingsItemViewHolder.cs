7004e3072a5017ee7217fd2692c36094eccf39d1
= <mako_edvin@yahoo.com>
2019-06-23 11:29:48 +0300

    Initial commit

---
+++ firstxamarindroid/Helpers/SettingsItemViewHolder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Com.Lilarcor.Cheeseknife;
+
+namespace firstxamarindroid.Helpers
+{
+    public class SettingsItemViewHolder : RecyclerView.ViewHolder
+    {
+        public SettingsItemViewHolder(View view) : base(view)
+        {
+            Cheeseknife.Inject(this, view);
+        }
+    }
+}
---
+++ firstxamarindroid/Helpers/SettingsListAdapter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Android.Support.V7.Widget;
+using Android.Views;
+
+namespace firstxamarindroid.Helpers
+{
+    public class SettingsListAdapter : RecyclerView.Adapter
+    {
+        public SettingsListAdapter()
+        {
+        }
+
+        public override int ItemCount => throw new NotImplementedException();
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
