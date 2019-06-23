bb916cc3b8da5ff8a866a5dba1073cc6df218145
= <mako_edvin@yahoo.com>
2019-06-23 23:03:39 +0300

    Added color picker dependencies

---
+++ firstxamarindroid/Adapters/LightsAdapter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Android.Support.V7.Widget;
+using Android.Views;
+
+namespace firstxamarindroid.Adapters
+{
+    public class LightsAdapter : RecyclerView.Adapter
+    {
+        public LightsAdapter()
+        {
+
+        }
+
+        public override int ItemCount => throw new NotImplementedException();
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ firstxamarindroid/Models/LightModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace firstxamarindroid.Models
+{
+    public class LightModel
+    {
+        private Boolean _status;
+        private int _brightness;
+
+
+        public LightModel()
+        {
+        }
+    }
+}
